1a495fc4709517e9f0eebac086d22a51c837b706
Responsive-web <saharsaeed8090@gmail.com>
2025-04-24 07:53:52 -0700

    login-signup-with react,tailwind and firebase

---
+++ signup-login/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Regestration from './Components/Regestration/Regestration'
+
+function App() {
+  return (
+    <div>
+      <Regestration />
+    </div>
+  )
+}
+
+export default App
